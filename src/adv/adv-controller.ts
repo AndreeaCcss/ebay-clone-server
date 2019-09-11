bdb00d0c6b9b456bbaadffc226a1ea6855bba634
andreeaCcss <andreeaccss@gmail.com>
2019-09-11 10:56:19 +0200

    added post and get endpoints and db model

---
+++ src/adv/adv-controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post, HttpCode, Body } from "routing-controllers";
+import Adv from "./entity";
+
+@Controller()
+export default class AdvController {
+  @Get("/advs")
+  async allAdvs() {
+    const advs = await Adv.find();
+    return { advs };
+  }
+
+  @Post("/advs")
+  @HttpCode(201)
+  createAdv(@Body() adv: Adv) {
+    return adv.save();
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import "reflect-metadata";
+import { createKoaServer } from "routing-controllers";
+import AdvController from "./adv/adv-controller";
+import setupDb from "./db";
+
+const port = process.env.PORT || 4000;
+
+const app = createKoaServer({
+  controllers: [AdvController]
+});
+
+setupDb()
+  .then(_ => app.listen(port, () => console.log(`Listening on port ${port}`)))
+  .catch(err => console.error(err));
